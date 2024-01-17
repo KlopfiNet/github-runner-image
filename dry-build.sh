bafe14b947c96e3cab1b1e9e72eb3dccd2a01b31
ThisIsntTheWay <msblauwal6@gmail.com>
2024-01-17 15:54:33 +0000

    Separate images for custom and summerwind runner

--- dry-build.sh
+++ dry-build.sh
@@ -4,4 +4,6 @@ gh_runner_version=$(curl --silent "https://api.github.com/repos/actions/runner/r
             sed -E 's/.*"([^"]+)".*/\1/' |
             sed -E 's/v//')
 export DOCKER_BUILDKIT=1
-docker buildx build --platform=linux/arm64 --build-arg="GH_RUNNER_VERSION=$gh_runner_version" . -t gh-runner:test
+
+echo "User supplied path of dockerfile: $1"
+docker buildx build --platform=linux/arm64 --build-arg="GH_RUNNER_VERSION=$gh_runner_version" $1 -t gh-runner:test
